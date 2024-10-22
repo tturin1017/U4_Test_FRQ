e0d60ea69df03e6871b1f381d1c10c9d033d7fc4
Tatiana Turin <146445767+tturin1017@users.noreply.github.com>
2024-10-22 13:24:19 -0400

    Initial commit

---
+++ src/main/java/com/example/project/SpinnerGame.java
@@ -0,0 +1,26 @@
+package com.example.project;
+
+public class SpinnerGame{
+    public SpinnerGame(){}
+
+    /*Precondition: min < max
+    Simulates a spin of a spinner by returning a random integer
+    between min and max, inclusive
+    */
+    public int spin(int min, int max){
+    /* to be implemented in part (a) */ 
+    }
+
+    /** Simulates one round of the game as described in part (b).
+    */
+    public void playRound(int playerSpin1, int computerSpin1, int playerSpin2, int computerSpin2){ 
+        /* to be implemented in part (b) */
+
+        /*It is difficult to test the output of this method with random values 
+        I will have to use the 4 parameters above. They should integrate into your method
+        seamlessly...You can however, CHANGE THE NAMES of the parameters to work with your method!
+        I will be checking your FRQ response to make sure you declared
+        and initialized your variables and assigned them to a call from your spin() method
+        */
+    }
+}
